d3320383faeb5ae01fb8ba7084f259db11f59095
Rafael Bonini <rboninidesouza@student.fullsail.edu>
2018-10-03 21:27:28 -0300

    project added

---
+++ BoniniRafael_ReleaseCandidate/app/src/main/java/boninirafael_releasecandidate/rafaelbonini/example/com/boninirafael_releasecandidate/MainActivity.java
@@ -0,0 +1,31 @@
+package boninirafael_releasecandidate.rafaelbonini.example.com.boninirafael_releasecandidate;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button managerButton, startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        managerButton = findViewById(R.id.manager_button);
+
+
+        managerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,ManagerActivity.class));
+            }
+        });
+    }
+}
